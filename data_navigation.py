f3c903223f15babbb87bcad7e45607a1a55819b5
jackrlynn <jacklynn@uiowa.edu>
2021-11-01 17:30:36 -0500

    Added scientific name to abbreviation conversion tool

--- data_navigation.py
+++ data_navigation.py
@@ -1,1 +1,17 @@
-# Add all data navigation functions here
+import pandas as pd
+
+# Dictionary that converts scientific names (case sensitive) to abbreviated names
+convSciToAbbr = {}
+
+def initializeDataTools():
+
+    # Add abbreviation conversions
+    abbr = pd.read_csv("data/species_abbr.csv")
+    abbr_lst = abbr["Abbr."]
+    sci_lst = abbr["Scientific Name"]
+    for i in range(len(abbr_lst)):
+        convSciToAbbr[sci_lst[i]] = abbr_lst[i]
+
+
+if __name__ == '__main__':
+    initializeDataTools()
