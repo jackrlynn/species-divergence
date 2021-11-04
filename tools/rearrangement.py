9ac147fb4b4a6ab82f79ca8a6ad397225cacc38c
jackrlynn <jacklynn@uiowa.edu>
2021-11-03 22:03:22 -0500

    Added some tests for SMASH

---
+++ tools/rearrangement.py
@@ -0,0 +1,4 @@
+# Reference: Maximillian Westphal, David Frankhouser, Carmine Sonzone, Peter G. Shields, Pearlly Yan, and Ralf Bundschuh.
+#            SMaSH: Sample matching using SNPs in humans. BMC Genomics 20(Suppl 12): 1001 (2019).
+#            https://doi.org/10.1186/s12864-019-6332-7.
+
