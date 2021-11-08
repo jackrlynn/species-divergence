014d5e4cf2335fe659f6c3345a524ddc2ed68890
jackrlynn <jacklynn@uiowa.edu>
2021-11-08 00:34:27 -0600

    Added species divergence method

--- dataNavigation.py
+++ dataNavigation.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-# Dictionary that converts scientific names (case sensitive) to abbreviated names
-convSciToAbbr = {}
+def getSpeciesDivergence(species_1, species_2):
 
-def initializeDataTools():
-
-    # Add abbreviation conversions
+    # Get abbreviation
+    sci_to_abbr = {}
     abbr = pd.read_csv("data/species_abbr.csv")
     abbr_lst = abbr["Abbr."]
     sci_lst = abbr["Scientific Name"]
+    i = 0
     for i in range(len(abbr_lst)):
-        convSciToAbbr[sci_lst[i]] = abbr_lst[i]
+        sci_to_abbr[sci_lst[i]] = [abbr_lst[i], i]
+        i += 1
 
-
-if __name__ == '__main__':
-    initializeDataTools()
+    # Get divergence matrix
+    diverg_mat = pd.read_csv("data/species_diverg.csv")
+    return diverg_mat[sci_to_abbr[species_1][0]][sci_to_abbr[species_2][1]]
