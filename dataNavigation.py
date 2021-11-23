bd1e8af8b5e3302246ad0c74b997c14d7e533bc6
jackrlynn <jacklynn@uiowa.edu>
2021-11-23 11:06:52 -0600

    Fixed file names

--- dataNavigation.py
+++ dataNavigation.py
@@ -1,10 +1,14 @@
+import os
+
 import pandas as pd
 
 def getSpeciesDivergence(species_1, species_2):
 
+    #working_dir = os.getcwd()
+
     # Get abbreviation
     sci_to_abbr = {}
-    abbr = pd.read_csv("data/species_abbr.csv")
+    abbr = pd.read_csv("/data/species_abbr.csv")
     abbr_lst = abbr["Abbr."]
     sci_lst = abbr["Scientific Name"]
     i = 0
@@ -13,7 +17,7 @@ def getSpeciesDivergence(species_1, species_2):
         i += 1
 
     # Get divergence matrix
-    diverg_mat = pd.read_csv("data/species_diverg.csv")
+    diverg_mat = pd.read_csv("/data/species_diverg.csv")
     return diverg_mat[sci_to_abbr[species_1][0]][sci_to_abbr[species_2][1]]
 
 def parseFasta(file_name, loc="./data/"):
--- tests/openFileTests.py
+++ tests/openFileTests.py
@@ -4,8 +4,16 @@ import dataNavigation as nav
 
 class TestStringMethods(test.TestCase):
 
-    def allSpeciesIncludedTest(self):
-        directory = 'data/hfe/training/'
+    def testAllSpeciesIncludedTraining(self):
+        directory = '/data/hfe/training/'
         for file in os.listdir(directory):
             file_name = file.replace('.txt', '')
             self.run(nav.getSpeciesDivergence(file_name, 'Artibeus jamaicensis'))
+
+    def testAllSpeciesIncludedTesting(self):
+        directory = '/data/hfe/testing/'
+        for file in os.listdir(directory):
+            f1 = file.replace('.txt', '')
+            for file2 in os.listdir(directory):
+                f2 = file2.replace('.txt', '')
+                self.assertTrue(nav.getSpeciesDivergence(f1, f2) > -1)
